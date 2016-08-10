c76dcdc55a5eded9936b0089482e3c881ebbd5ee
Taff Gao <gaotongfei1995@gmail.com>
2016-08-10 20:26:14 +0800

    update setup.py version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='loot',
-    version='0.1.4',
+    version='0.2.0',
     author='Taff Gao',
     author_email='gaotongfei199@gmail.com',
     url='https://github.com/gaotongfei/loot',
